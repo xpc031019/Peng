550139d3b767b3ca4e0587c41c1703c08a6b0244
xpc031019 <160452075+xpc031019@users.noreply.github.com>
2024-02-21 09:29:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,26 @@ from flask import Flask,request,render_template
 
 app=Flask(__name__)
 
+r=""
+first_time=1
 @app.route("/",methods=["GET","POST"])
 def index():
   return(render_template("index.html"))
 
-@app.route（“/main"，methods=["GET","POST"]）
-def main（）：
-r=request.form.get("r")
-return(render_template("main.html",r=r))
-
-@app.route("/imageGPT",methods=["GET","POST"])
+@app.route("/main",methods=["GET","POST"])
 def main():
-  return(render_template("imageGPT.html,r=r))
-                         
-         if__name__=="__main__"：
-  app.run()
+  r=request.form.get("r")
+  return(render_template("main.html",r=r))
+
+@app.route("/image_gpt",methods=["GET","POST"])
+def image_gpt():
+  return(render_template("image_gpt.html"))
+
+@app.route("/end",methods=["GET","POST"])
+def end():
+  first_time=1
+  return(render_template("end.html"))
+
+  if __name__=="__main__":
+         app.run()
   
